2192a99d4ed6767ee83ed62b2eae8647cf464516
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-05 15:30:13 +0200

    Change random number

--- src/Generator/FbpGenerator.php
+++ src/Generator/FbpGenerator.php
@@ -17,7 +17,8 @@ final class FbpGenerator implements FbpGeneratorInterface
         $creationTime = ceil(microtime(true) * 1000);
 
         if (null === $random) {
-            $random = (string) random_int(1_000_000_000, 9_999_999_999);
+            // from inspecting existing fbp values it seems that the random part is a number between 1,000,000,000 and 1,999,999,999
+            $random = (string) random_int(1_000_000_000, 1_999_999_999);
         }
 
         return sprintf('fb.1.%s.%s', $creationTime, $random);
