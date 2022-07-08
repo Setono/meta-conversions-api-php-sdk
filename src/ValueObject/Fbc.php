96c082c12e0d64a345b834a51b4b9ee374d04afb
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-08 15:58:39 +0200

    Fix code style

--- src/ValueObject/Fbc.php
+++ src/ValueObject/Fbc.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Setono\MetaConversionsApi\ValueObject;
 
-use Webmozart\Assert\Assert;
-
 /**
  * See https://developers.facebook.com/docs/marketing-api/conversions-api/parameters/fbp-and-fbc#fbc
  */
