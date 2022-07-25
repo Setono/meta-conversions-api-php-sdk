1871f6b7f10724d4f8d911a04b5e8890b52654ff
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-25 12:41:09 +0200

    Add @internal

--- src/Client/ErrorResponse.php
+++ src/Client/ErrorResponse.php
@@ -11,6 +11,8 @@ use Webmozart\Assert\Assert;
  * todo also handle a JSON response like this:
  *
  * {"error":{"message":"Invalid parameter","type":"OAuthException","code":100,"error_subcode":2804050,"is_transient":false,"error_user_title":"Du har ikke tilf\u00f8jet tilstr\u00e6kkelige data om kundeoplysningsparametre for denne h\u00e6ndelse","error_user_msg":"Denne h\u00e6ndelse har ingen kundeoplysningsparametre, eller den har en kombination af kundeoplysningsparametre, der er s\u00e5 bred, at det er usandsynligt, at det vil v\u00e6re effektivt til matchning. Du kan l\u00f8se dette ved at g\u00e5 til de anbefalede fremgangsm\u00e5der for parametre p\u00e5 developers.facebook.com\/docs\/marketing-api\/conversions-api\/best-practices\/#req-rec-params","fbtrace_id":"Asu2mk752HZ0oT07IksFAwN"}}
+ *
+ * @internal
  */
 final class ErrorResponse
 {
