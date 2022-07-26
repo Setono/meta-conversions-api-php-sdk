613d9c29e2e82e9a1c27d6c4d31ab1bbddf76627
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-26 10:26:23 +0200

    Update comment

--- src/Client/ClientInterface.php
+++ src/Client/ClientInterface.php
@@ -8,7 +8,7 @@ use Setono\MetaConversionsApi\Event\Event;
 use Setono\MetaConversionsApi\Exception\ClientException;
 
 /**
- * Implement this interface in a client that is able to send the conversion api event to a Facebook endpoint
+ * Implement this interface in a client that is able to send the conversion api event to a Meta/Facebook endpoint
  */
 interface ClientInterface
 {
