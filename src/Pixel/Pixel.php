6e67a4c6074f48f46a7dbec503655e4c7b319447
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-26 10:31:40 +0200

    Add comment about nullable access token

--- src/Pixel/Pixel.php
+++ src/Pixel/Pixel.php
@@ -8,6 +8,14 @@ final class Pixel
 {
     public string $id;
 
+    /**
+     * The reasons for the access token being nullable:
+     *
+     * 1. If you only want to use client side tracking you don't need an access token
+     * 2. It's not necessarily the case that the access token is present when you create the event,
+     *    but first available when you want to send the event, hence you populate it later
+     * 3. There's the risk of having the access token being outputted in some user facing error message
+     */
     public ?string $accessToken;
 
     public function __construct(string $id, string $accessToken = null)
