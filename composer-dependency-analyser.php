defd81e26e4c01a7f73b36a000c3fa6839079448
Joachim Løvgaard <joachim@loevgaard.dk>
2025-04-14 13:53:45 +0200

    Add false positives

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('psr/http-client-implementation', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-factory-implementation', [ErrorType::UNUSED_DEPENDENCY])
 ;
